f94a428f09d131a9ca8b9e812d0bb2b93cf202e3
diabetesdt1 <schifti@spam-hosting.com>
2017-04-21 13:48:30 +0200

    Update config.inc.php

--- config.inc.php
+++ config.inc.php
@@ -9,4 +9,4 @@ $config['client']             = 'Google-Fit-Importer';
 $config['timeout']            = 5;   # Nightsout api timeout in secornds
 $config['mindauer']           = 10;  # only data, which was longer then xx Minutes
 $config['minmeters']          = 500; # or only data with more then xxx meters
-$config['secure']             = 2;   # 2 = ignore Cert-Failures, 1 = strict ssl-cert checking
+$config['secure']             = 1;   # 2 = ignore Cert-Failures, 1 = strict ssl-cert checking
